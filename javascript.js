453fdf0e0d43f79c1d9caf4562bfee6c63e42aaa
Ankita Binu <ankitabinu1998@gmail.com>
2022-02-09 17:52:01 -0500

    Editted button text

---
+++ javascript.js
@@ -0,0 +1,23 @@
+// let counter=0;
+
+// function myStart(){
+//     let counterHTML=document.getElementById("counter");
+//     counterHTML.innerHTML=++counter;
+// }
+
+// function myStop(){
+//     counter=0;
+//     let counterHTML=document.getElementById("counter");
+//     counterHTML.innerHTML=counter;
+// }
+
+
+function buttonText(){
+    if (document.getElementById("Button").innerHTML==="Start"){
+        document.getElementById("Button").innerHTML="Stop";
+     
+    }else{
+        document.getElementById("Button").innerHTML="Start";
+        
+    }
+}
