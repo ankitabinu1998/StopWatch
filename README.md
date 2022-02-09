39f3ab7136ed2388b1ab04d9df48830da2cb68b7
Ankita Binu <ankitabinu1998@gmail.com>
2022-02-09 18:48:05 -0500

    StopWatch working

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# Timer
+# StopWatch
 Step one: create a button 
 Step two: change button from Start to Stop when pressed
+Step three: increment a counter per second until Stop is pressed
+
+To Do
+Add CSS ...try Bootstrap
--- javascript.js
+++ javascript.js
@@ -1,23 +1,32 @@
-// let counter=0;
 
-// function myStart(){
-//     let counterHTML=document.getElementById("counter");
-//     counterHTML.innerHTML=++counter;
-// }
-
-// function myStop(){
-//     counter=0;
-//     let counterHTML=document.getElementById("counter");
-//     counterHTML.innerHTML=counter;
-// }
+setInterval(myIncr,1000);
+let counter=0;
 
 
 function buttonText(){
     if (document.getElementById("Button").innerHTML==="Start"){
         document.getElementById("Button").innerHTML="Stop";
+        
+        
      
     }else{
         document.getElementById("Button").innerHTML="Start";
         
     }
+
 }
+function myIncr(){
+    
+    if(document.getElementById("Button").innerHTML==="Stop"){    
+        document.getElementById("counter").innerHTML=counter++ +" s";
+    }else{
+        counter=0;
+        document.getElementById("counter").innerHTML=counter +" s";
+    }
+
+}
+
+
+
+
+
